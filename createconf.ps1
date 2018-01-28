77fab399ac795407edd237cca812a62d67f4451e
Rimvydas <lithstud@gmail.com>
2018-01-28 16:19:13 +0200

    Creates smartcash.conf

---
+++ createconf.ps1
@@ -0,0 +1,8 @@
+﻿#generate random user and pass
+$RandUser = -join ([char[]](65..90+97..122)*100 | Get-Random -Count 5)
+$RandPass = -join ([char[]](65..90+97..122)*100 | Get-Random -Count 32)
+
+$Settings = "rpcuser=$RandUser
+rpcpassword=$RandPass
+txindex=1"
+$Settings | Out-File -FilePath $Env:AppData/Smartcash/smartcash.conf -NoNewline -Encoding utf8
