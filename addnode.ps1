3666128c67f1c007d01e3bead68ff646ceac814b
Rimvydas <lithstud@gmail.com>
2018-01-28 16:15:33 +0200

    Script to add node info to smartnode.conf

---
+++ addnode.ps1
@@ -0,0 +1,9 @@
+﻿$Label = Read-Host -Prompt 'Node Label'
+$IP = Read-Host -Prompt 'VPS IP'
+$genkey = Read-Host -Prompt 'GenKey (SmartnodePrivKey)'
+$outputH = Read-Host -Prompt 'Output hash'
+$index = Read-Host -Prompt 'Output Index'
+
+$Settings = ("
+$Label {0}:9678 $genkey $outputH $index" -f $IP)
+$Settings | Out-File -FilePath $Env:AppData/Smartcash/smartnode.conf -NoNewline -Encoding utf8 -Append
